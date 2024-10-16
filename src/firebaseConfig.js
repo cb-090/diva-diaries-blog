c9332ebd1f7edd8927d394b82407c04a602f680e
cb-090 <cbabin2005@gmail.com>
2024-10-16 13:39:22 -0700

    Updated gitignore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,7 +8,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   //FIREBASE CONFIG HERE 
+    apiKey: "AIzaSyAzKNYGRXZQwwvWgmDJ-NJdLdiPQMNittM",
+    authDomain: "basic-blog-844d9.firebaseapp.com",
+    projectId: "basic-blog-844d9",
+    storageBucket: "basic-blog-844d9.appspot.com",
+    messagingSenderId: "276229865572",
+    appId: "1:276229865572:web:a01a0808a56fda85d25f71"
 };
 
 export const app = initializeApp(firebaseConfig)
