3335528b893f82f12762325c207d5796f5a0e99c
cb-090 <cbabin2005@gmail.com>
2024-10-21 13:46:16 -0700

    Added authentication architecture and article database

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Blog App
 
-This is a basic blog app, created with Vite and a starter repo. All credit for this project will go to **CMSI 2021 Homework 3**.
+This is a basic blog app, created with Vite and a starter repo that uses React and Firebase. All credit for this project will go to **CMSI 2021 Homework 3**.
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,26 @@ import { useEffect, useState } from "react"
 import Nav from "./Nav"
 import Article from "./Article"
 import ArticleEntry from "./ArticleEntry"
-import { SignIn, SignOut, useAuthentication } from "../services/authService"
+import { SignIn, SignOut } from "./Auth"
+import { useAuthentication } from "../services/authService"
 import { fetchArticles, createArticle } from "../services/articleService"
 import "./App.css"
 
 export default function App() {
   const [articles, setArticles] = useState([])
   const [article, setArticle] = useState(null)
-  const [writing, setWriting] = useState(null)
+  const [writing, setWriting] = useState(false)
   const user = useAuthentication()
 
-  // This is a trivial app, so just fetch all the articles once, when
-  // the app is loaded. A real app would do pagination. Note that
+  // This is a trivial app, so just fetch all the articles only when
+  // a user logs in. A real app would do pagination. Note that
   // "fetchArticles" is what gets the articles from the service and
   // then "setArticles" writes them into the React state.
-
   useEffect(() => {
     if (user) {
       fetchArticles().then(setArticles)
     }
   }, [user])
-  
-  useEffect(() => {
-    fetchArticles().then(setArticles)
-  }, [])
 
   // Update the "database" *then* update the internal React state. These
   // two steps are definitely necessary.
